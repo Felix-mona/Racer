4e546acea24c675efd23ce88ddf7c22087dd183d
Felix <felix_mona11@outlook.com>
2024-11-06 15:27:46 +0000

    V1.01
    Currently setting up documentation info and started testing/changing figures to see where the issue lies within the camera's movement.

--- Assets/Scripts/CameraFollowPlayerScript.cs
+++ Assets/Scripts/CameraFollowPlayerScript.cs
@@ -13,8 +13,11 @@ public class CameraFollowPlayerScript : MonoBehaviour
     public Vector3 offset;
 
     //Two values that are used for the smoothing and speed of the camera's movement
-    public float smoothSpeed = 0.125f;
-    public float followSpeed = 5.0f;
+    public float smoothSpeed = 0.22f;
+    public float followSpeed = 160f;
+
+    public float offSetBehind = 6.0f;
+    public float offSetAbove = 2.0f;
     private Vector3 velocity = Vector3.zero;
 
     // Update is called once per frame
@@ -23,12 +26,21 @@ public class CameraFollowPlayerScript : MonoBehaviour
         //find the instantiated car prefab
         playerCar = GameObject.FindGameObjectWithTag("Player Car");
         //Get the 'Desired' position of the car ready to move the camera's position to it
-        Vector3 desiredPosition = playerCar.transform.position - playerCar.transform.forward * 5.0f + playerCar.transform.up * 2.0f;
+        Vector3 desiredPosition = playerCar.transform.position - playerCar.transform.forward * offSetBehind + playerCar.transform.up * offSetAbove;
+
+
         //transform the desired position to make the camera's vector move with a smoother movement
         Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed, followSpeed);
         //Move the camera and change the way it is facing (at the car)
         transform.position = smoothedPosition;
         transform.LookAt(playerCar.transform);
+
+
+
+
+
+        /* if the camera is more than a certain distance away set the smoothspeed to 0
+        if it is under the threshold use the smoothspeed like normal*/
     }
 
 }
